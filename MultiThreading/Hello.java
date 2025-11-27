4ec71307cd1afcc1ce2998841a37f9b41722633b
Pradip-Garai <pradipgarai173@gmail.com>
2025-11-27 18:50:41 +0530

    First Thread Runs

---
+++ MultiThreading/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String as[]){
+
+        World w = new World();
+        w.start();
+        
+        for(;;){
+            System.out.println("Hello");
+        }
+    }
+}
